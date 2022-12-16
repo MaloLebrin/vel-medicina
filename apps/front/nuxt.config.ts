933a3ae027b30c520fdc1d153cd9225220abafcf
malolebrin <malolebrin@gmail.com>
2022-12-16 21:58:33 +0100

    :wrench: config heroicons

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,4 +1,30 @@
+import Components from 'unplugin-vue-components/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { type PluginOption } from 'vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [
+          name => {
+            if (name.includes('IconSolid')) {
+              const realName = name.split('Solid')[0]
+              return `@heroicons/vue/24/solid/esm/${realName}.js`
+            }
+            if (name.includes('IconOutline')) {
+              const realName = name.split('Outline')[0]
+              return `@heroicons/vue/24/outline/esm/${realName}.js`
+            }
+          },
+        ],
+        include: [/\.vue$/, /\.vue\?vue/],
+        extensions: ['vue'],
+      }),
+
+      visualizer() as PluginOption,
+    ],
+  },
 
 })
