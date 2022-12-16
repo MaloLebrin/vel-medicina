b6601824c96cfc5a6c5d43e56f7f866a74df306c
malolebrin <malolebrin@gmail.com>
2022-12-16 22:04:30 +0100

    🚀 add defaults layouts and pages

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -4,6 +4,8 @@ import { type PluginOption } from 'vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: { pageTransition: { name: 'page', mode: 'out-in' } },
+
   vite: {
     plugins: [
       Components({
